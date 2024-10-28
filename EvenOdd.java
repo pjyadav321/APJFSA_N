940188811fd0aadc67f89b502781988ff9f08be4
pjyadav321 <pjyadav321@gmail.com>
2024-10-28 18:36:21 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,24 @@
+// WAP to Check whether given number is even or odd
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      //Prompt user to take the input
+      System.out.println("Enter a integer number: ");
+      int n = sc.nextInt();
+
+      // Check number is even or odd
+      if(n%2 == 0){
+          System.out.println("Number is even.");
+      }
+      else {
+          System.out.println("Number is odd.");
+      }
+    }
+}
+/*
+    OUTPUT:
+        Enter a integer number:
+        40
+        Number is even.
+ */
