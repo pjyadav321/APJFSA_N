03142db1a57f99b17f9b502f16a37b580307826a
pjyadav321 <pjyadav321@gmail.com>
2024-12-12 20:17:47 +0530

    Add files via upload

--- ReversingArrayList.java
+++ ReversingArrayList.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 public class ReversingArrayList {
     public static void main(String  args[]){
         ArrayList <Integer> ar= new ArrayList<>();
+        //Adding elements in arrayList
         ar.add(3);
         ar.add(4);
         ar.add(5);
@@ -14,10 +15,16 @@ public class ReversingArrayList {
 
         System.out.println("Original List: "+ar);
 
-        Collections.reverse(ar);
+        Collections.reverse(ar); // Reversing the elements of original array
         System.out.println("Reversed List: "+ar);
 
         Collections.sort(ar);
-        System.out.println("Sorted list: "+ar);
+        System.out.println("Sorted list: "+ar); //Sorting the elements in increasing order
     }
 }
+
+/*
+        Original List: [3, 4, 5, 6, 87, 2, 4]
+        Reversed List: [4, 2, 87, 6, 5, 4, 3]
+        Sorted list: [2, 3, 4, 4, 5, 6, 87]
+ */
